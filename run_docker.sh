f407d8b59f40690edbc957ec91f174525ab742e4
rostyslavhereha <rostyslav.hereha@lvairo.com>
2025-03-31 15:17:31 +0300

    add contatiner name

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,5 @@
-docker run --network host -w /DEIM \
+docker run --name deim_train \
+  --network host -w /DEIM \
   -v /data/DEIM:/DEIM \
-  -v /data/dd_dataset:/data/dd_dataset \
+  -v /data:/data \
   --gpus '"device=0"' --shm-size=8g -it quay.io/logivations/ml_all:deim
