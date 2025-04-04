27133cd0d88a9dfb464bb354939884d0e7e11c87
rostyslavhereha <rostyslav.hereha@lvairo.com  git config --global user.name rostyslavherehagit config --global user.email rostyslav.hereha@lvairo.com>
2025-04-04 15:20:52 +0000

    chage paths   && removing contatiner

--- run_docker.sh
+++ run_docker.sh
@@ -2,4 +2,5 @@ docker run --name deim_train \
   --network host -w /DEIM \
   -v /data/DEIM:/DEIM \
   -v /data:/data \
-  --gpus all --shm-size=8g -it quay.io/logivations/ml_all:deim
+  --rm --gpus all \
+  --shm-size=8g -it quay.io/logivations/ml_all:deim
